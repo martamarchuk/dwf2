fd7b0065e2f133055b5e9723d6180b4b507cff24
igorip2005 <70385821+igorip2005@users.noreply.github.com>
2025-10-12 14:10:11 +0700

    Updated segment-group.tsx

--- src/components/ui/segment-group.tsx
+++ src/components/ui/segment-group.tsx
@@ -15,12 +15,12 @@ export default function CatalogueSegmentGroup({ value, onValueChange, options }:
         orientation="horizontal"
         className="flex gap-1 bg-slate-200 relative p-1.5 rounded-xl shadow-inner"
       >
-        <SegmentGroup.Indicator className="bg-orange-500 z-10 rounded-lg shadow-lg h-(--height) w-(--width) transition-all duration-300" />
+        <SegmentGroup.Indicator className="bg-white z-10 rounded-lg shadow-lg h-(--height) w-(--width) transition-all duration-300" />
         {options.map((option) => (
           <SegmentGroup.Item
             key={option}
             value={option}
-            className="flex flex-1 items-center justify-center select-none cursor-pointer text-sm font-semibold px-4 py-3.5 z-20 text-slate-500 hover:text-slate-700 data-[state=checked]:text-white data-disabled:cursor-not-allowed data-disabled:opacity-40 transition-all duration-300"
+            className="flex flex-1 items-center justify-center text-center select-none cursor-pointer text-sm font-semibold px-4 py-3.5 z-20 text-slate-500 hover:text-slate-700 data-[state=checked]:text-orange-500 data-disabled:cursor-not-allowed data-disabled:opacity-40 transition-all duration-300"
           >
             <SegmentGroup.ItemText>{option}</SegmentGroup.ItemText>
             <SegmentGroup.ItemControl />
